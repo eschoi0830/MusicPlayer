284e3d47738bedd171b164d470fe59f8edb52279
eschoi0830 <eschoi0830@gmail.com>
2024-06-18 14:41:14 +0900

    처음 프로젝트 구성

---
+++ libs/linkedlist.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "linkedlist.h"
+
+static Node* _head = NULL;
+static Node* _tail = NULL;
+static Node* _cur_node = NULL;
+
+bool empty(){
+   return false;
+}
+
+size_t size() {
+    return 0;
+}
+
+void print(){
+    
+}
+
+void print_file(FILE* stream){
+    
+}
+
+void clear(){
+    
+}
+
+Node* append_left(size_t n, char new_data[]){
+    return NULL;
+}
+
+Node* append(size_t n, char new_data[]){
+    return NULL;
+}
+
+Node* _insert_after(Node* cur_node, size_t n, char new_data[]){
+    return NULL;
+}
+
+Node* insert_after(Node* cur_node, Node* new_node){
+    return NULL;
+}
+
+Node* pop_left(){
+    return NULL;
+}
+
+Node* pop(){
+    return NULL;
+}
+
+Node* delete_node(Node* cur_node){
+    return NULL;
+}
+
+Node* delete_by_data(char* data){
+    return NULL;
+}
+
+Node* next(){
+    return NULL;
+}
+
+Node* prev(){
+    return NULL;
+}
+
+Node* first_node(){
+    return NULL;
+}
+
+Node* last_node(){
+    return NULL;
+}
+
+Node* get_node(size_t index){
+    return NULL;
+}
+
---
+++ libs/textfilewriter.c
@@ -0,0 +1,19 @@
+// textfilewriter.h
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "linkedlist.h"
+#include "textfilewriter.h"
+
+void create_music_titles(FILE* stream){
+    printf("Test\n");
+}
+
+void read_file(char* file_name){
+
+}
+
+void write_file(char* file_name){
+
+}
